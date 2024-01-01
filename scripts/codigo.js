8889ae4ea45f723ea5a177375c8a6cfac72ae3ff
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2024-01-01 12:42:36 -0500

    :dizzy: Animaçāo menu de codigo

--- scripts/codigo.js
+++ scripts/codigo.js
@@ -2,13 +2,26 @@ var botaoCode = document.getElementById('botaoCode')
 var botaoCodex = document.getElementById('botaoCodeX')
 
 var secCodigo = document.getElementById('codigo')
+var codigoBg = document.getElementById('codigoBg')
+var codigoCard = document.getElementById('codigoCard')
 
 botaoCode.addEventListener('click', () => {
     secCodigo.classList.remove('hide')
+    codigoBg.classList.add('opacidadeAlta')
+    codigoBg.classList.remove('opacidadeBaixa')
+    codigoCard.classList.add('menuEntrando')
+    codigoCard.classList.remove('menuSaindo')
 })
 
 botaoCodex.addEventListener('click', () => {
-    secCodigo.classList.add('hide')
+    codigoBg.classList.add('opacidadeBaixa')
+    codigoBg.classList.remove('opacidadeAlta')
+    codigoCard.classList.add('menuSaindo')
+    codigoCard.classList.remove('menuEntrando')
+    setTimeout(() => {
+        secCodigo.classList.add('hide');
+    }, 1000);
+
 })
 
 
